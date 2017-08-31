84037d94646d1fde162232785c0323f5ec21935c
unknown <Greg.Marshall.Hill@gmail.com>
2017-08-31 13:28:18 -0500

    Functional Shopping Cart project completed. Added better comments

--- lib.js
+++ lib.js
@@ -24,23 +24,29 @@ const cart =
   })
 
 /**
- * should return an array with the `itemName` repeated `count` number of times
+ * @name itemRepeater
+ * @param itemName the name of the item to repeat
+ * @param count the number of times to repeat the item name in the array
+ * @returns an array with the `itemName` repeated `count` number of times
  */
 const itemRepeater =
   itemName =>
-    count => {
-      // TODO
-    }
+    count => Array(count).fill(itemName)
 
 /**
- * should return an array of carts with each given customer's shopping list
- * as an array of items
+ * @name constructCarts
+ * @param listings the item/price pairs
+ * @param customers an array of customer objects which contain the customer's name and an object shoppingList
+ * @returns an array of carts with each given customer's shopping list as an array of items
  */
 const constructCarts =
   listings =>
-    customers => {
-      // TODO
-    }
+    customers =>
+      customers.map(customer => ({
+        customer: customer.name,
+        items: entries(customer.shoppingList).reduce((listCtor, item) =>
+          listCtor.concat(itemRepeater(item[0])(item[1])), [])
+      }))
 
 module.exports = {
   listing,
