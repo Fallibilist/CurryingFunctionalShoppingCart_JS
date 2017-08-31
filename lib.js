69202127e51272c8882cd3561d79101161437414
unknown <Greg.Marshall.Hill@gmail.com>
2017-08-31 14:35:35 -0500

    Added JSDoc Comments

--- lib.js
+++ lib.js
@@ -24,7 +24,9 @@ const cart =
   })
 
 /**
- * @name itemRepeater
+ * @function itemRepeater
+ * @author Greg Hill
+ * @description creates an array of count size with the string itemName as the value of each element
  * @param itemName the name of the item to repeat
  * @param count the number of times to repeat the item name in the array
  * @returns an array with the `itemName` repeated `count` number of times
@@ -34,7 +36,9 @@ const itemRepeater =
     count => Array(count).fill(itemName)
 
 /**
- * @name constructCarts
+ * @function constructCarts
+ * @author Greg Hill
+ * @description constructs an an array of cart objects which list the items in the customer's shopping list
  * @param listings the item/price pairs
  * @param customers an array of customer objects which contain the customer's name and an object shoppingList
  * @returns an array of carts with each given customer's shopping list as an array of items
@@ -42,11 +46,9 @@ const itemRepeater =
 const constructCarts =
   listings =>
     customers =>
-      customers.map(customer => ({
-        customer: customer.name,
-        items: entries(customer.shoppingList).reduce((listCtor, item) =>
-          listCtor.concat(itemRepeater(item[0])(item[1])), [])
-      }))
+      customers.map(customer =>
+        (cart(customer.name, ...entries(customer.shoppingList).reduce((listCtor, item) =>
+          listCtor.concat(itemRepeater(item[0])(item[1])), []))))
 
 module.exports = {
   listing,
